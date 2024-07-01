aff237cae69c78895926ff00e0aa725d09944529
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-07-01 13:47:49 +0100

    new branch creation

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ const express = require("express")
 
 const app = express();
 
-app.use("/activityPages", express.static("activityPages"))
-app.use("/example", express.static("example"));
+
 
 app.listen(5001, () => {
     console.log(`Server is listen of port 5001`);
